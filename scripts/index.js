502f047b6066176f40ffd3f798429a73198470a8
Izabella Chebakova <chebakovaizabella@gmail.com>
2022-06-14 17:11:58 +0300

    Правки в js, отображении header, profile, добавление списка в elements, исправление опечаток

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,30 @@ const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 const saveButton = popup.querySelector('.popup__save-button');
+const formElement = popup.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__field_type_name');
+const jobInput = formElement.querySelector('.popup__field_type_description');
+const namePlace = document.querySelector('.profile__name');
+const descriptionPlace = document.querySelector('.profile__subtitle');
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-saveButton.addEventListener('click', closePopup);
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = namePlace.textContent;
+  jobInput.value = descriptionPlace.textContent;
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-const formElement = popup.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__name');
-const jobInput = formElement.querySelector('.popup__description');
-const namePlace = document.querySelector('.profile__name');
-const descriptionPlace = document.querySelector('.profile__subtitle');
-
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
   namePlace.textContent = nameInput.value;
   descriptionPlace.textContent = jobInput.value;
+  closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
